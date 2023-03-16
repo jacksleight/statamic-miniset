6550c03feca99cdd45fa100d66bb82f50790da12
Jack Sleight <jacksleight@gmail.com>
2023-03-16 18:06:55 +0000

    WIP

--- resources/js/addon.js
+++ resources/js/addon.js
@@ -1,6 +1,4 @@
-/* @deprecated: Alias the Statamic v4 scales so the same classes can work in v3 as well, drop when v3 is dropped */
 import '../css/addon.css';
-import '../css/jstw.css';
 
 import MinisetFieldtype from './components/Fieldtypes/MinisetFieldtype.vue';
 import MinisetClassesFieldtype from './components/Fieldtypes/MinisetClassesFieldtype.vue';
